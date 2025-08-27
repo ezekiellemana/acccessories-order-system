abe84161d65958e4ee9d589f7f794ff8277a2c9c
EnzoLeman <ezekielaugustino@gmail.com>
2025-08-27 21:18:31 +0300

    fix vulnerbility

--- astore-frontend/src/firebase/config.js
+++ astore-frontend/src/firebase/config.js
@@ -1,20 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCkr54G4ak9ZU6hXx3JcSpdjEuQaRniieU",
-  authDomain: "astore-96dac.firebaseapp.com",
-  projectId: "astore-96dac",
-  storageBucket: "astore-96dac.firebasestorage.app",
-  messagingSenderId: "153527661938",
-  appId: "1:153527661938:web:3234f200ac52e0b6f059eb",
-  measurementId: "G-8V694H3J8P"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
+
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app);
